4cc4b4a568cf50ad030d48c5c21d71ac61fffb35
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-24 10:58:46 +0900

    12.3　衝突判定を共通化（メソッド化）

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -46,7 +46,7 @@ public class GameFrame extends MyFrame{
 	public void checkPlayerAndEnemies() {
 		for(int i=0; i<GameWorld.enemies.size();i++) {
 			Enemy e=GameWorld.enemies.get(i);
-			if(Math.abs(e.x-GameWorld.player.x)<=30 && Math.abs(e.y-GameWorld.player.y)<=30) {
+			if(checkHit(GameWorld.player, e)) {
 				System.out.println("やられた！");
 				GameWorld.player.y=-1000;
 			}
@@ -60,7 +60,7 @@ public class GameFrame extends MyFrame{
 			int hits=0;
 			while(j<GameWorld.enemies.size()) {
 				Enemy e= GameWorld.enemies.get(j);
-				if(Math.abs(e.x-b.x)<=30 && Math.abs(e.y-b.y)<=30){
+				if(checkHit(e, b)){
 					System.out.println("あたり");
 					hits++;
 					GameWorld.enemies.remove(j);
@@ -73,6 +73,13 @@ public class GameFrame extends MyFrame{
 			}else {
 				i++;
 			}
+		}
+	}
+	public boolean checkHit(Character a, Character b) {
+		if(Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30){
+			return true;
+		}else {
+			return false;
 		}
 	}
 
