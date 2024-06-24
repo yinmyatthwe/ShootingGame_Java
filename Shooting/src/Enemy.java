faa5767a6176178338a40dc301e9db898889029c
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-24 11:07:05 +0900

    12.4 敵に耐久力をつける

--- Shooting/src/Enemy.java
+++ Shooting/src/Enemy.java
@@ -1,5 +1,7 @@
 
 public class Enemy extends Character {
+	int life=3;
+	
 	public Enemy(double x, double y, double vx, double vy) {
 		super(x,y,vx,vy);
 	}
--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -63,6 +63,9 @@ public class GameFrame extends MyFrame{
 				if(checkHit(e, b)){
 					System.out.println("あたり");
 					hits++;
+					e.life--;
+				}
+				if(e.life<=0){
 					GameWorld.enemies.remove(j);
 				}else {
 					j++;
