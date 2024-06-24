eb09cd48ac14dfb63ae710d3ca173e5135f26c69
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-24 11:11:16 +0900

    12.5　敵の種類によって耐久力を交換する

--- Shooting/src/Enemy.java
+++ Shooting/src/Enemy.java
@@ -1,6 +1,6 @@
 
 public class Enemy extends Character {
-	int life=3;
+	int life=1;
 	
 	public Enemy(double x, double y, double vx, double vy) {
 		super(x,y,vx,vy);
--- Shooting/src/EnemyBase.java
+++ Shooting/src/EnemyBase.java
@@ -8,6 +8,7 @@ public class EnemyBase extends Enemy{
 	}
 	public EnemyBase(double x, double y, double vx, double vy) {
 		super(x,y,vx,vy);
+		life=20;
 	}
 	public void move() {
 		super.move();
