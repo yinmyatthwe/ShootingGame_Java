28d0007a6daf91c8ca915b977b8524b647c2b568
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-24 12:13:48 +0900

    13.1　すべての敵を倒したら「クリア」を表示する

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -16,7 +16,10 @@ public class GameFrame extends MyFrame{
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
-			
+			if(GameWorld.enemies.size()==0) {
+				setColor(0, 0, 0);
+				drawString("クリア", 100,200,40);
+			}
 			sleep(0.03);
 		}
 		
@@ -41,6 +44,15 @@ public class GameFrame extends MyFrame{
 			Enemy e=GameWorld.enemies.get(i);
 			e.draw(this);
 			e.move();
+		}
+		int i=0;
+		while(i<GameWorld.enemies.size()) {
+			Enemy e= GameWorld.enemies.get(i);
+			if(e.y>400) {
+				GameWorld.enemies.remove(i);
+			}else {
+				i++;
+			}
 		}
 	}
 	
@@ -80,7 +92,7 @@ public class GameFrame extends MyFrame{
 		}
 	}
 	public boolean checkHit(Character a, Character b) {
-		if(Math.abs(a.x-b.x)<=20 && Math.abs(a.y-b.y)<=20){
+		if(Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30){
 			return true;
 		}else {
 			return false;
