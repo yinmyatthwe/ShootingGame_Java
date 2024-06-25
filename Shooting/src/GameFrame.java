2c43fe786f0aae84b2c09f9318f2d4e3868e0c26
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-06-25 09:56:04 +0900

    Enterキーを押さたらもう一度ゲームを実行する

--- Shooting/src/GameFrame.java
+++ Shooting/src/GameFrame.java
@@ -4,35 +4,38 @@ public class GameFrame extends MyFrame{
 	public void run() {
 		GameWorld.player=new Player(100, 300, 0, 0);
 		addKeyListener(GameWorld.player);
-		GameWorld.playerBullets = new Vector<PlayerBullet>();
-		GameWorld.enemies = new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100, 50, 10, 0));
-		GameWorld.enterPressed=false;
-		
 		while(true) {
-			clear();
-			GameWorld.player.draw(this);
-			GameWorld.player.move();
-			movePlayerBullets();
-			moveEnemies();
-			checkPlayerAndEnemies();
-			checkPlayerBulletsAndEnemies();
-			if(GameWorld.enemies.size()==0) {
-				setColor(0, 0, 0);
-				drawString("クリア", 100,200,40);
-				if(GameWorld.enterPressed) {
-					break;
-				}
-			} else if (GameWorld.player.y<0) {
-				setColor(0, 0, 0);
-				drawString("ゲームオーバー", 50, 200, 40);
-				if(GameWorld.enterPressed) {
-					break;
-				}
-			}
-			sleep(0.03);
-		}
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
+			GameWorld.playerBullets = new Vector<PlayerBullet>();
+			GameWorld.enemies = new Vector<Enemy>();
+			GameWorld.enemies.add(new EnemyBase(100, 50, 10, 0));
+			GameWorld.enterPressed=false;
 		
+			while(true) {
+				clear();
+				GameWorld.player.draw(this);
+				GameWorld.player.move();
+				movePlayerBullets();
+				moveEnemies();
+				checkPlayerAndEnemies();
+				checkPlayerBulletsAndEnemies();
+				if(GameWorld.enemies.size()==0) {
+					setColor(0, 0, 0);
+					drawString("クリア", 100,200,40);
+					if(GameWorld.enterPressed) {
+						break;
+					}
+				} else if (GameWorld.player.y<0) {
+					setColor(0, 0, 0);
+					drawString("ゲームオーバー", 50, 200, 40);
+					if(GameWorld.enterPressed) {
+						break;
+					}
+				}
+				sleep(0.03);
+			}
+		}
 	}
 	public void movePlayerBullets() {
 		int i=0;
